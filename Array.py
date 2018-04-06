fc632909c9c63bdd141e2ef4a5ef11c6636e5a73
rameeeen23 <35401793+rameeeen23@users.noreply.github.com>
2018-04-07 00:22:13 +0800

    Add files via upload

---
+++ Array.py
@@ -0,0 +1,11 @@
+#JOHN DAVE B. FUERTE
+
+#Array
+
+#github.com/rameeeen23
+
+#johndavefuerte@gmail.com
+
+dave = ["Curse", "of", "Nazarick", "Overlord", ",", "Ainz", "Ooal", "Gown"]
+del dave[1]
+print (dave)
